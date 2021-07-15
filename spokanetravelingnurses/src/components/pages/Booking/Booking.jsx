81c9aa73abbf5e89cff3d4bce9b61c6fcee4c88e
Kyla Oyamot <koyamot@gmail.com>
2021-07-15 15:23:21 -0700

    change booking-container padding

--- spokanetravelingnurses/src/components/pages/Booking/Booking.jsx
+++ spokanetravelingnurses/src/components/pages/Booking/Booking.jsx
@@ -4,33 +4,43 @@ import { Typography } from "antd";
 
 const { Title } = Typography;
 
-
 const Booking = () => {
-
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs
-    .sendForm("spokanetravelingnurses", "requestforconsult", e.target, "user_68C11RQOE7GkU008gS3Gj")
-    .then(
-      (result) => {
-        console.log(result.text);
-      },
-      (error) => {
-        console.log(error.text);
-      }
-    );
+      .sendForm(
+        "spokanetravelingnurses",
+        "requestforconsult",
+        e.target,
+        "user_68C11RQOE7GkU008gS3Gj"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
 
     e.target.reset();
   };
 
   return (
-    <div style={{ background: "#FFF" }}>
+    <div
+      className="booking-container"
+      style={{
+        backgroundColor: "white",
+        minHeight: "100vh",
+        padding: "2.4rem 8rem 2.4rem 8rem",
+      }}
+    >
       <Title level={1}>Booking</Title>
       <form onSubmit={sendEmail}>
         <label>
           First Name:
-          <input type="text" placeholder="First Name" name="firstName"/>
+          <input type="text" placeholder="First Name" name="firstName" />
         </label>
         <label>
           Last Name:
@@ -38,18 +48,22 @@ const Booking = () => {
         </label>
         <label>
           Email Address:
-          <input type="email" placeholder="E-mail address" name="email"/>
+          <input type="email" placeholder="E-mail address" name="email" />
         </label>
         <label>
           Phone Number:
-          <input type="tel" placeholder="" name="ph_number"/>
+          <input type="tel" placeholder="" name="ph_number" />
         </label>
         <p>Dates interested in:</p>
         <div>
-        <input type="date" name="startDate"/> - <input type="date" name="endDate"/>
+          <input type="date" name="startDate" /> -{" "}
+          <input type="date" name="endDate" />
         </div>
-        <label> Send a message(Any information the property manager may need to know, accomodations, pets, car, etc):
-            <br></br>
+        <label>
+          {" "}
+          Send a message(Any information the property manager may need to know,
+          accomodations, pets, car, etc):
+          <br></br>
           <textarea type="text" name="message"></textarea>
         </label>
         <button type="submit">Send</button>
