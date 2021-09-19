ad4ce27ce1cc8f3639a0e26c20aa9b9dacfa4e2f
Kyla Oyamot <koyamot@gmail.com>
2021-09-18 23:04:50 -0700

    Edit landing

--- spokanetravelingnurses/src/components/pages/Landing/Landing.js
+++ spokanetravelingnurses/src/components/pages/Landing/Landing.js
@@ -17,14 +17,30 @@ const Landing = () => {
           }}
         >
           <Title level={1}>Traveling Nurse Communal Living</Title>
+          <Paragraph>
+            Spokane Traveling Nurses is ran by a husband and wife with years of
+            property management experience. They take pride in their properties,
+            with more than 10 rental properties under their care. The couple has
+            decided to flip their home of 25 years into a welcoming space for
+            traveling nurses visiting Spokane, WA.
+          </Paragraph>
           <Image src={img} preview={false} />
           <Title level={4}>708 S. Thor St. Spokane, WA 99202</Title>
-          <Title level={3} style={{ fontStyle: "italic" }}><i>YOUR HOME AWAY FROM HOME</i></Title>
+          <Title level={3} style={{ fontStyle: "italic" }}>
+            <i>YOUR HOME AWAY FROM HOME</i>
+          </Title>
           <Paragraph>
-            Enjoy your stay in this fully remodeled, cozy home centrally located in the City of Spokane. With fully furnished bedrooms, an at-home gym, a complete kitchen, and shared living and dining rooms, Spokane Traveling Nurses set the standard for communal living. Your stay also includes on-site laundry, a koi pond and patio, dog run, and plenty of off-street parking.
+            Enjoy your stay in this fully remodeled, cozy home centrally located
+            in the City of Spokane. With fully furnished bedrooms, an at-home
+            gym, a complete kitchen, and shared living and dining rooms, Spokane
+            Traveling Nurses set the standard for communal living. Your stay
+            also includes on-site laundry, a koi pond and patio, dog run, and
+            plenty of off-street parking.
           </Paragraph>
           <Paragraph>
-            In each room we provide a TV with DishTV, a refridgerator and freezer, a vanity, a mirror with shelving, a dresser, a queen-sized bed with fresh bedding, and a bluetooth headset.
+            In each room we provide a TV with DishTV, a refridgerator and
+            freezer, a vanity, a mirror with shelving, a dresser, a queen-sized
+            bed with fresh bedding, and a bluetooth headset.
           </Paragraph>
         </section>
       </div>
