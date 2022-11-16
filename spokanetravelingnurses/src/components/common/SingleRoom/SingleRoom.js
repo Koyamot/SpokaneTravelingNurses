da1a03c96934bb260dfb4f601fac4545c06cd761
koyamot <koyamot@gmail.com>
2022-11-16 12:05:03 -0800

    fixed pictures for rooms.

--- spokanetravelingnurses/src/components/common/SingleRoom/SingleRoom.js
+++ spokanetravelingnurses/src/components/common/SingleRoom/SingleRoom.js
@@ -1,41 +1,18 @@
 import Paragraph from "antd/lib/typography/Paragraph";
 import React, { useState, useRef} from "react";
-import { useQuery, gql } from "@apollo/client";
 import { Link, useLocation, useParams } from "react-router-dom";
 import Title from "antd/lib/typography/Title";
 
-const GET_SINGLE_ROOM = gql`
-  query getSinglePublicRoom($id: ID!) {
-    publicRoom(id: $id) {
-      id
-      room_name
-      description
-      link {
-        link
-      }
-      Pictures {
-        id
-        formats
-      }
-    }
-  }
-`;
 
-function SingleRoom({ amenities }) {
+function SingleRoom({ amenities, room }) {
   const [copied, setCopied] = useState(false);
   const textAreaRef = useRef(null);
   const { id } = useParams();
   const location = useLocation();
-  const { loading, error, data } = useQuery(GET_SINGLE_ROOM, {
-    variables: {
-      id: id,
-    },
-  });
+  const roomInfo = room[id]
+
   const [featured, setFeatured] = useState("");
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>DOH! :(</p>;
-  const image = `${data.publicRoom.Pictures[0].formats.small.url}`;
   let authButton;
   if (location.pathname.includes("admin")) {
     authButton = (
@@ -50,11 +27,9 @@ function SingleRoom({ amenities }) {
       </Link>
     );
   }
-
-  const room = data.publicRoom;
-
-  const pics = room.Pictures.map((pics) => {
-    const link = `${pics.formats.medium.url}`;
+  const image = roomInfo.pictures[0]
+  const pics = roomInfo.pictures.map((pics) => {
+    const link = `${pics}`;
     return link;
   });
 
@@ -72,7 +47,7 @@ function SingleRoom({ amenities }) {
   }
   return (
     <div className="container single-room">
-      <Title>{room.room_name}</Title>
+      <Title>{roomInfo.name}</Title>
       <div className="flex-row">
         <div className="flex-column justify-center pic-options">
           {pics.map((link, i) => (
@@ -91,17 +66,19 @@ function SingleRoom({ amenities }) {
           </div>
         )}
       </div>
-      <Paragraph>{room.description}</Paragraph>
+      {/* <Paragraph>{room.description}</Paragraph> */}
+      <div className="margin-top">
       <Title level={4}>Each room includes:</Title>
       <div className="flex-column amenities-list">
         {amenities.map((item, i) => (
           <Paragraph key={i}>{item}</Paragraph>
         ))}
       </div>
+      </div>
       <div className="flex-column align-center copy-link-box">
         <Title level={4}>Share this room:</Title>
         <div className="flex-row align-baseline">
-          <input value={room.link.link} ref={textAreaRef} readOnly />
+          <input value={`${roomInfo.link}${id}`} ref={textAreaRef} readOnly />
           <button className="copy-button" onClick={copy}>
             Copy Link
           </button>
--- spokanetravelingnurses/src/components/pages/Rooms/Rooms.jsx
+++ spokanetravelingnurses/src/components/pages/Rooms/Rooms.jsx
@@ -1,43 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useQuery, gql } from "@apollo/client";
-
-const PUBLIC_ROOMS = gql`
-  query getPublicRooms {
-    publicRooms {
-      id
-      room_name
-      roomNumber {
-        id
-        Occupied
-      }
-      id
-      Pictures {
-        id
-        formats
-      }
-    }
-  }
-`;
 
 const RoomList = ({ rooms }) => {
-  const { loading, error, data } = useQuery(PUBLIC_ROOMS, {});
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>DOH! :(</p>;
-  const room = data.publicRooms.map((item) => item);
-
   return (
     <div className="container room-list">
-      {room.map((room) => {
-        return !room.roomNumber.Occupied ? (
+      {rooms.map((room) => {
+        return (
           <Link to={`/room/${room.id}`} key={room.id}>
             <Rooms room={room} />
           </Link>
-        ) : (
-          <div className="disable" key={room.id}>
-            <Rooms room={room} />
-          </div>
         );
       })}
     </div>
@@ -48,12 +19,12 @@ const Rooms = ({ room }) => {
   return (
     <div key={room.id} className="room-card">
       <img
-        src={room.Pictures[0].formats.small.url}
+        src={room.pictures[0]}
         className="room-pic"
         alt="rooms"
       />
       <div className="room-info">
-        <p>{room.room_name}</p>
+        <p>{room.name}</p>
       </div>
     </div>
   );
